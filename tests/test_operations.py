5dc7207c54e8083ac1a7d18ad7a9a668bff9e2ac
TS0713 <tsp.0713@gmail.com>
2024-12-16 17:32:50 +0530

    Added files

---
+++ tests/test_operations.py
@@ -0,0 +1,11 @@
+from src.math_operations import add,minus
+
+def test_add():
+    assert add(2,3)==5
+    assert add(1,-1)==0
+
+def test_minuns():
+    assert minus(5,3)==2
+    assert minus(1,1)==0
+
+
